6bdc7aa03209195ec46509d1df4a693058dda686
dduohereshui <2245675739@qq.com>
2022-09-21 22:47:30 +0800

    trimleft添加trimright

--- medium/TrimLeft.ts
+++ medium/TrimLeft.ts
@@ -4,4 +4,9 @@ type TrimLeft<S extends string> = S extends `${" " | "\n" | "\t"}${infer Rest}`
   ? TrimLeft<Rest>
   : S;
 
+type TrimRight<S extends string> = S extends `${infer Rest}${" " | "\n" | "\t"}`
+  ? TrimRight<Rest>
+  : S;
+
+type trimed1 = TrimRight<"  Hello World  ">;
 export {};

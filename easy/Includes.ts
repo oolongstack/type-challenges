9670c72747ec612da7fac723009374ddd52aa427
dduohereshui <2245675739@qq.com>
2022-09-23 10:37:13 +0800

    Includes

--- easy/Includes.ts
+++ easy/Includes.ts
@@ -3,12 +3,12 @@
 type Equal<A, B> = A extends B ? (B extends A ? true : false) : false;
 
 // 推测的使用 第一次 A 推测为Kars，B推测为数组中其他的值组成的数组，第二次 A 推测为 Esidisi ...
-type Includes<T extends readonly any[], U> = T extends [infer A, ...infer B]
+type Includes<T extends any[], U> = T extends [infer A, ...infer Rest]
   ? Equal<A, U> extends true
     ? true
-    : Includes<B, U>
+    : Includes<Rest, U>
   : false;
 
-type isPillarMen = Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Dio">; // expected to be `false`
+type isPillarMen = Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Wamuu">; // expected to be `false`
 
 export {};
